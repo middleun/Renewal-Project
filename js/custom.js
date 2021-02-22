6b11bfd1d685188c338d9e9c616892226537b9b4
middleun <eunallaco@gmail.com>
2021-02-22 18:42:50 +0900

    210222_슬라이더 실행 함수 작업중

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,15 @@ $(function(){
 
     // Main Slider and Button
     $(".slider").on(function(){
-        let index=$(this).index();
-        $(".sliderDots li").eq(index).addClass("on");
+        // let index=$(this).index();
+        $(".sliderDots li").eq(0).addClass("on");
+
+        
+        // $(".sliderDots li").find("button").removeClass("active");
+        // $(".sliderDots li").find("button").eq(index).addClass("active");
+
+        // $(".sliderDots li").find("button").eq(0).addClass("active");
+
 
 
     });
