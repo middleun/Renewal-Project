26ff6b0f30dee517994410f46aa2cf67a6538b14
middleun <eunallaco@gmail.com>
2021-02-23 15:44:32 +0900

    210223_스크롤시 헤더 고정

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,44 @@
 $(function(){
 
+    // Fixed Header
+
+    const offTop = $("#header").offset().top;
+
     
+        $(window).scroll(function(){
+            let scroll = $(window).scrollTop();
+            if(offTop <= scroll){
+                $("#header").css({"position":"fixed","background":"#fff"});
+                
+                
+            }else{
+                $("#header").css({"position":"relative", "background":"#fcf7ef"});
+            }
+        });
+       
+      
+    
+
+
+    // topNavBar Click
+    $(this).hasClass("click");
+    const navBar=function(){
+        $(".topNavBar").click(function(){
+            $(this).toggleClass("click");
+            if($(this).hasClass("click")){
+                $(this).find("i").attr("class","fas fa-times");
+                // $(".hiddenNav").show("fast");
+            }else{
+                $(this).find("i").attr("class", "fas fa-bars");
+                // $(".hiddenNav").hide("fast");
+
+            }
+        });
+    }
+    navBar();
+
+
+     
 
     // Main Slider and Button
     $(".slider").on(function(){
