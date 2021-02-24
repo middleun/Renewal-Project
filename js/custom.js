42e99c640622949e6f15c9225192088d10480411
middleun <eunallaco@gmail.com>
2021-02-24 12:47:28 +0900

    210224_스크롤바 스타일 / 그리드섹션 작업중 / resize시 비율유지 작업중

--- js/custom.js
+++ js/custom.js
@@ -2,25 +2,22 @@ $(function(){
 
     // Fixed Header
 
-    const offTop = $("#header").offset().top;
-
-    
-        $(window).scroll(function(){
-            let scroll = $(window).scrollTop();
-            if(offTop <= scroll){
-                $("#header").css({"position":"fixed","background":"#fff"});
-                
-                
-            }else{
-                $("#header").css({"position":"relative", "background":"#fcf7ef"});
-            }
-        });
-       
-      
+    const offTop = $("#header").offset().top;    
+    $(window).scroll(function(){
+        let scroll = $(window).scrollTop();
+        if(offTop <= scroll){
+            $("#header").css({"position":"fixed","background":"#fff"});
+            
+            
+        }else{
+            $("#header").css({"position":"relative", "background":"#fcf7ef"});
+        }
+    });      
     
 
 
     // topNavBar Click
+
     $(this).hasClass("click");
     const navBar=function(){
         $(".topNavBar").click(function(){
@@ -37,6 +34,42 @@ $(function(){
     }
     navBar();
 
+    // Resize 비율유지 
+    // $(document).ready(function(){
+    //     $("body").width($(window).width());
+    //     $("body").height($(window).height());
+
+    //     $(window).resize(function(){
+    //         $("body").width($(window).width()-20);
+    //         $("body").height($(window).height()-20);
+    //     });
+    // });
+
+
+
+
+    // 3차(비율계산해서 나누기)
+    // $(window).resize(function(){
+    //     let winWidth = $(window).width();
+    //     // var sliderWidth= winWidth*3.6;
+    //     // var sliderWidth = $('.slider').width();
+        
+    //     if(winWidth <= 1200){
+    //         //1.681:1
+    //         $('.slider').width(winWidth/3);
+    //     }
+    // });
+
+    // 2차
+    // $(window).resize(function(){
+    //     let winWidth = $(window).width();
+    //     if(winWidth >= 800){
+    //       $(".navigation").css({"display":"block"});
+    //      } else {
+    //        $(".navigation").css({"display":"none"});
+    //      }
+     
+    //    });
 
      
 
